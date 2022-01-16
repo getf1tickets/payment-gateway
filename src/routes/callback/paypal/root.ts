580790696116549cabac846a2c1296124cd63c1c
iverly <iverly.dev@gmail.com>
2022-01-16 20:15:33 +0100

    feat: add cancel paypal callback

--- src/routes/callback/paypal/root.ts
+++ src/routes/callback/paypal/root.ts
@@ -64,6 +64,62 @@ const root: FastifyPluginAsync = async (fastify): Promise<void> => {
       reply.redirect(302, `${process.env.WWW_PAYMENT_SUCCESS_REDIRECT_URL}?orderId=${order.id}`);
     },
   });
+
+  fastify.route<{
+    Querystring: {
+      token: string,
+    }
+  }>({
+    method: 'GET',
+    url: '/cancel',
+    handler: async (request, reply) => {
+      if (!request.query.token) {
+        throw fastify.httpErrors.badRequest();
+      }
+
+      const order = await fastify.to500<Order>(Order.findOne({
+        include: [
+          {
+            model: Payment,
+            as: 'payment',
+            required: true,
+            include: [
+              {
+                model: PaymentMethod,
+                as: 'method',
+                required: true,
+                where: {
+                  transactionId: request.query.token,
+                },
+              },
+            ],
+          },
+        ],
+      }));
+
+      if (!order) {
+        throw fastify.httpErrors.badRequest();
+      }
+
+      if (order.status !== OrderStatus.WAITING_PAYMENT) {
+        throw fastify.httpErrors.conflict();
+      }
+
+      await fastify.to500(order.payment.method.update({
+        status: 'CANCELED',
+      }));
+
+      await fastify.to500(order.payment.update({
+        status: PaymentStatus.CANCELLED,
+      }));
+
+      await fastify.to500(order.update({
+        status: OrderStatus.CANCELLED,
+      }));
+
+      reply.redirect(302, `${process.env.WWW_PAYMENT_CANCEL_REDIRECT_URL}`);
+    },
+  });
 };
 
 export default root;
